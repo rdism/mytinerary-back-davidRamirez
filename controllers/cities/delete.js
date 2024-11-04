d867b37bf8dbc59178c48fcb2a95dd1e8c0217f9
rdism <86022701+rdism@users.noreply.github.com>
2024-11-04 18:50:39 -0300

    Se crea el código para el controlador de deleteCities y el router

--- controllers/cities/delete.js
+++ controllers/cities/delete.js
@@ -1,8 +1,18 @@
-import City from "../../models/City";
+import City from "../../models/City.js";
 
 const deleteCity = async (req,res,next) => {
     try {
-        
+        let { id } = req.params;
+        let deletedCity = await City.findByIdAndDelete(id);
+
+        if (!deletedCity) {
+            next();
+        }
+
+        return res.status(200).json({
+            message: "Ciudad eliminada con éxito",
+            city: deletedCity
+        });
     } catch (error) {
         next(error)
     }
--- router/cities.js
+++ router/cities.js
@@ -1,9 +1,14 @@
 import { Router } from "express";
 import { allCities, cityByID } from "../controllers/cities/read.js";
+import { deleteCity } from "../controllers/cities/delete.js";
 
 const routerCities = Router();
 
+// Rutas para obtener ciudades
 routerCities.get('/all', allCities);
 routerCities.get('/id/:id', cityByID);
+
+// Ruta para eliminar una ciudad por ID
+routerCities.delete('/id/:id', deleteCity);
 
 export default routerCities;
